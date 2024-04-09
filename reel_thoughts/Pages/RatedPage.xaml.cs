d79d95479738bc81fe4a364ebbea7eea7a241d5d
W0301241 <W0301241@nscc.ca>
2024-04-09 10:48:36 -0300

    added pages

---
+++ reel_thoughts/Pages/RatedPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace reel_thoughts.Pages
+{
+    /// <summary>
+    /// Interaction logic for RatedPage.xaml
+    /// </summary>
+    public partial class RatedPage : Page
+    {
+        public RatedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
